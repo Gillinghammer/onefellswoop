0aadabb8eca299370669a0ba2fe99f1bd1910f60
Gillinghammer <colingillingham@gmail.com>
2016-11-15 17:59:41 +0000

    created seperate routers for http and api requests

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // server.js
 var express     = require('express');
 var mongoose    = require('mongoose');
-var router      = require('./routes');
+var apiRouter   = require('./routes/apiRoutes');
+var router      = require('./routes/routes');
 var morgan      = require('morgan');
+var path        = require('path');
 
 // connect to db
 mongoose.connect('mongodb://localhost:27017/onefellswoop')
@@ -15,11 +17,19 @@ db.once('open', function() {
 });
 
 // define app
-var app         = express(); 
+var app         = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
 var bodyParser  = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true  }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+
+// Router middleware
+apiRouter.use(function(req, res, next) {
+  console.log('something is happening.');
+  next();
+});
 
 // Router middleware
 router.use(function(req, res, next) {
@@ -27,12 +37,8 @@ router.use(function(req, res, next) {
   next();
 });
 
-// test api
-router.get('/', function(req, res) {
-  res.json({  message: 'hooray! welcome to our api!'  });
-});
-
-app.use('/api', router)
+app.use('/api', apiRouter)
+app.use('/', router)
 
 // Start the server
 var port = process.env.PORT || 8888;  // set our port
