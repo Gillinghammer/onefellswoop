15372482976ba83cc7d35b0e8800819acb1e24e5
Gillinghammer <colingillingham@gmail.com>
2016-11-17 14:20:41 +0000

    Employer profile search and basic results page

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express     = require('express');
 var mongoose    = require('mongoose');
 var apiRouter   = require('./routes/apiRoutes');
 var router      = require('./routes/routes');
-var morgan      = require('morgan');
+// var morgan      = require('morgan');
 var path        = require('path');
 
 // connect to db
@@ -23,7 +23,7 @@ app.set('view engine', 'pug')
 var bodyParser  = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true  }));
 app.use(bodyParser.json());
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 
 // Router middleware
 apiRouter.use(function(req, res, next) {
@@ -34,6 +34,12 @@ apiRouter.use(function(req, res, next) {
 // Router middleware
 router.use(function(req, res, next) {
   console.log('something is happening.');
+  next();
+});
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
